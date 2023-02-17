efbb0d6d5062abdea7779e15fc1eb5f1be819bea
SungGyunKim <egoksg@naver.com>
2023-02-18 01:13:28 +0900

    Spring Boot가 DataSource를 설정하려는데 설정값이 없어서 오류가 발생하여 자동 설정 해제

--- src/main/java/com/egoksg/migration/SpringMigrationApplication.java
+++ src/main/java/com/egoksg/migration/SpringMigrationApplication.java
@@ -2,8 +2,9 @@ package com.egoksg.migration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringMigrationApplication {
 
 	public static void main(String[] args) {
