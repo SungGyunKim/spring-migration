b118aa21690612447a31bdfb8c4f4b5bcb2875c5
SungGyunKim <egoksg@naver.com>
2023-02-18 22:03:27 +0900

    batch용 SqlSession 설정, TransactionManager 설정

--- src/main/java/com/egoksg/migration/db1/model/TestDb1Dto.java
+++ src/main/java/com/egoksg/migration/db1/model/TestDb1Dto.java
@@ -1,7 +1,9 @@
 package com.egoksg.migration.db1.model;
 
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class TestDb1Dto {
 	Integer no;
--- src/test/java/com/egoksg/migration/TestDb1MapperTest.java
+++ src/test/java/com/egoksg/migration/TestDb1MapperTest.java
@@ -1,10 +1,18 @@
 package com.egoksg.migration;
 
+import java.util.List;
+
+import org.apache.ibatis.executor.BatchResult;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;import org.springframework.boot.test.mock.mockito.ResetMocksTestExecutionListener;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.egoksg.migration.db1.mapper.TestDb1Mapper;
+import com.egoksg.migration.db1.model.TestDb1Dto;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -14,8 +22,22 @@ public class TestDb1MapperTest {
 	@Autowired
 	private TestDb1Mapper testDb1Mapper;
 	
+	@Autowired
+	@Qualifier("db1BatchSqlSessionTemplate")
+	public SqlSession db1BatchSqlSession;
+	
 	@Test
 	public void testDb1SelectTest() {
 		log.info(testDb1Mapper.select());
 	}
+	
+	@Test
+	@Transactional
+	@Commit// @Test에서 @Transactional에서 commit을 하지 않아 수동으로 해줘야 한다. ref) https://www.inflearn.com/questions/257700
+	public void testDb1InsertBatchTest() {
+		for (int i = 0; i < 1_000_000; i++) {
+			db1BatchSqlSession.insert("com.egoksg.migration.db1.mapper.TestDb1Mapper.insert", TestDb1Dto.builder().name("BATCH").build());
+		}
+	}
+	
 }
